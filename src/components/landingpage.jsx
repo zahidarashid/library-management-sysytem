3957187970ef7892fa67b207ad3afc8765574af6
Zahida Rashid <rehmatjan346@gmail.com>
2023-04-03 12:31:38 +0530

    added two links in landingpage

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Landingpage = () => {
     return ( 
         <div className="landingpage">
-            <h1>Login As....?</h1>
+            <h1>Signin As....?</h1>
+            <Link to="/admin-login">Admin Login</Link>
+            <Link to="/user-login">User Login</Link>
         </div>
      );
 }
